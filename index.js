bd9549daab3afada683e563ecf74b11e1dc41fc6
koivkai <kaius.koivunen@gmail.com>
2019-01-31 18:51:43 +0200

    part3 ex 3.2 3.3 3.4 3.5 3.6

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
  let persons = [
     {
@@ -25,12 +27,61 @@ const app = express()
   ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Puhelinluettelo</h1>')
 })
 
 app.get('/persons', (req, res) => {
   res.json(persons)
 })
+
+app.post('/persons', (request, response) => {
+  const person = request.body
+  console.log('person to add', person)
+  if(person.name === undefined || person.name === '') {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+  if(person.number === undefined || person.number === '') {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+console.log(persons.find(p => p.name === persons.name) === undefined)
+  if(persons.find(p => p.name === persons.name) === undefined) {
+    return response.status(400).json({
+      error: 'name already in contacts'
+    })
+  }
+  const randomID = Math.random() * 10000000000000000
+  person.id = randomID
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/info', (req, res) => {
+    let d = new Date()  
+    res.send(`Puhelinluettelossa on ${persons.length} henkilön tiedot <br/> ${d}`)
+  })
+
+app.get('/persons/:id', (req, res) => {
+    console.log('persons get id:', req.params.id)
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+    
+})
+
+app.delete('/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    person = persons.filter(p => p.id !== id); 
+    response.status(204).end();
+});
 
 const PORT = 3001
 app.listen(PORT, () => {
