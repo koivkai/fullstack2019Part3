e9bc6ecbebab67a4e8ba2fca3433d7600c6d3e06
koivkai <kaius.koivunen@gmail.com>
2019-02-02 19:21:28 +0200

    part3 ex 3.13 3.14

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -16,29 +17,31 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 app.use(cors())
 app.use(express.static('build'))
 
+const Contact = require('./models/contact')
 
- let persons = [
-    {
-      "name": "Frodo Baggings",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Sawise Gangee",
-      "number": "040-123456",
-      "id": 2
-    },
-    {
-      "name": "Meriadoc Brandybuck",
-      "number": "040-123456",
-      "id": 3
-    },
-    {
-      "name": "Peregrin Took",
-      "number": "040-123456",
-      "id": 4
-    }
-  ]
+
+//  let persons = [
+//     {
+//       "name": "Frodo Baggings",
+//       "number": "040-123456",
+//       "id": 1
+//     },
+//     {
+//       "name": "Sawise Gangee",
+//       "number": "040-123456",
+//       "id": 2
+//     },
+//     {
+//       "name": "Meriadoc Brandybuck",
+//       "number": "040-123456",
+//       "id": 3
+//     },
+//     {
+//       "name": "Peregrin Took",
+//       "number": "040-123456",
+//       "id": 4
+//     }
+//   ]
 
 app.get('/api', (req, res) => {
   res.send('<h1>Puhelinluettelo</h1>')
@@ -46,38 +49,52 @@ app.get('/api', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   console.log('pyydettiin persons')
-  res.json(persons)
+  Contact.find({}).then(returnedPersons => {
+    res.json(returnedPersons.map(person => person.toJSON()))
+  })
 })
 
-app.post('/api/persons', (request, response) => {
-  const person = request.body
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+
+  
   //console.log('person to add', person)
   //console.log('person name', person.name, ' person number', person.number)
   if(person.name === undefined || person.name === '') {
-    return response.status(400).json({
+    return res.status(400).json({
       error: 'name missing'
     })
   }
   if(person.number === undefined || person.number === '') {
-    return response.status(400).json({
+    return ReadableStream.status(400).json({
       error: 'number missing'
     })
   }
+
+  const newPerson = new Contact({
+    name: person.name,
+    number: person.number
+  })
+
+  newPerson.save().then(savedPerson => {
+    res.json(savedPerson.toJSON())
+  })
+
   //console.log('persons sisältö ', persons)
-  let found = persons.find(p => p.name !== persons.name)
+  //let found = persons.find(p => p.name !== persons.name)
   //console.log('löytyy henkilö ', found)
   //console.log('löytyy jo luettelosta?',persons.find(p => p.name === persons.name) === undefined)
-  if(persons.find(p => p.name === persons.name) !== undefined) {
-    return response.status(400).json({
-      error: 'name already in contacts'
-    })
-  }
-  //console.log('päästiin iffien läpi')
-  const randomID = Math.random() * 10000000000000000
-  person.id = randomID
-  persons = persons.concat(person)
-  //console.log('persons lopuksi',persons)
-  response.json(person)
+  // if(persons.find(p => p.name === persons.name) !== undefined) {
+  //   return res.status(400).json({
+  //     error: 'name already in contacts'
+  //   })
+  // }
+  // //console.log('päästiin iffien läpi')
+  // const randomID = Math.random() * 10000000000000000
+  // person.id = randomID
+  // persons = persons.concat(person)
+  // //console.log('persons lopuksi',persons)
+  // res.json(person)
 })
 
 app.get('/api/info', (req, res) => {
@@ -86,14 +103,22 @@ app.get('/api/info', (req, res) => {
   })
 
 app.get('/api/persons/:id', (req, res) => {
-    console.log('persons get id:', req.params.id)
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
+  Contact.findById(req.params.id).then(person => {
     if(person) {
-        res.json(person)
+      res.json(person.toJSON())
     } else {
-        res.status(404).end()
+      res.status(404).end()
     }
+  })
+
+    // console.log('persons get id:', req.params.id)
+    // const id = Number(req.params.id)
+    // const person = persons.find(p => p.id === id)
+    // if(person) {
+    //     res.json(person)
+    // } else {
+    //     res.status(404).end()
+    // }
     
 })
 
@@ -103,7 +128,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/contact.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('commecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+.then(result => {
+    console.log('succesfully conntected to MongoDB')
+})
+.catch((error) => {
+    console.log('Error encountered when connectiong to MongoDB: ',error.message)
+})
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+})
+
+contactSchema.set('toJSON', {
+  transform: (decument, returndObject) => {
+    returndObject.id = returndObject._id.toString()
+    delete returndObject._id
+    delete returndObject.__v
+  }
+})
+
+module.exports = mongoose.model('Contact', contactSchema)
